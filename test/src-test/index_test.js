9e472e96c0537dad496c6e0bfb65be51afe7710d
Eli White <github@eli-white.com>
2016-07-01 19:50:15 -0700

    Breaking: Updating deps and dropping support for node < 4 (#24)

--- test/src-test/index_test.js
+++ test/src-test/index_test.js
@@ -7,7 +7,7 @@ const cleanGitRef = require('../../src/index');
 function assertValidBranchName(branchName) {
   const checkRefFormat = spawn('git', ['check-ref-format', 'refs/' + branchName]);
 
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     checkRefFormat.on('close', (code) => {
       if (code === 0) {
         resolve();
@@ -21,24 +21,24 @@ function assertValidBranchName(branchName) {
 
 function assertOutputAndVerifyValid(input, output) {
   it("should convert '" + input + "' to '" + output + "'", () => {
-    var result = cleanGitRef.clean(input);
+    const result = cleanGitRef.clean(input);
     assert.strictEqual(result, output);
     return assertValidBranchName(result);
   });
 }
 
-describe('CleanGitRef', function() {
-  describe('clean', function() {
-    it('should throw if given a non string', function() {
-      assert.throws(function() {
+describe('CleanGitRef', () => {
+  describe('clean', () => {
+    it('should throw if given a non string', () => {
+      assert.throws(() => {
         cleanGitRef.clean(4);
       });
 
-      assert.throws(function() {
+      assert.throws(() => {
         cleanGitRef.clean({});
       });
 
-      assert.throws(function() {
+      assert.throws(() => {
         cleanGitRef.clean([]);
       });
     });
