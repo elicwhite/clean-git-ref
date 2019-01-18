00e0942296b424fe86675a793179bb1c8eca3fd5
Theodoor van Donge <theodoorvd@gmail.com>
2019-01-18 19:31:05 +0100

    Replace all instances of invalid characters (#31)
    * Make the sample in the README work.
    Also this fixes #30
    
    * Try to satisfy the Travis build
    
    * Fix NodeJS v4 error: TypeError: Cannot supply flags when constructing one RegExp from another

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 'use strict';
 
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+function replaceAll(str, search, replacement) {
+  search = search instanceof RegExp
+      ? search
+      : new RegExp(escapeRegExp(search), 'g');
+
+  return str.replace(search, replacement);
+}
+
 const CleanGitRef = {
   clean(value) {
     if (typeof value !== 'string') {
       throw new Error('Expected a string, received: ' + value);
     }
 
-    return value.replace('./', '/')
-    .replace('..', '.')
-    .replace(' ', '-')
-    .replace(/^[~^:?*\\\-]/, '')
-    .replace(/[~^:?*\\]/, '-')
-    .replace(/[~^:?*\\\-]$/, '')
-    .replace('@{', '-')
-    .replace(/\.$/, '')
-    .replace(/\/$/, '')
-    .replace(/\.lock$/, '');
+    value = replaceAll(value, './', '/');
+    value = replaceAll(value, '..', '.');
+    value = replaceAll(value, ' ', '-');
+    value = replaceAll(value, /^[~^:?*\\\-]/g, '');
+    value = replaceAll(value, /[~^:?*\\]/g, '-');
+    value = replaceAll(value, /[~^:?*\\\-]$/g, '');
+    value = replaceAll(value, '@{', '-');
+    value = replaceAll(value, /\.$/g, '');
+    value = replaceAll(value, /\/$/g, '');
+    value = replaceAll(value, /\.lock$/g, '');
+    return value;
   }
 };
 
